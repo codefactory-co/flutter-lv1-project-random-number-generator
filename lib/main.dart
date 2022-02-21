660c642b0e0e68863f361c9ca0dc161d6f1e4f13
JC <withj1123@gmail.com>
2022-02-21 15:45:08 +0900

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:random_number_generator/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
